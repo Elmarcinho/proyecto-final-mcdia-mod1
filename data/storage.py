686295fb077b3b280b17181c3671f7108a51386a
Elvis Miranda <elvis.miranda@live.com>
2025-04-16 23:06:18 -0400

    Agregar componente Storage a la solucion

---
+++ data/storage.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from models import ImageModel
+
+
+# Storage represents the dictionary (persistency)
+class Storage:
+    STORAGE = "storage.json"
+
+    def load_storage(self):
+        if not os.path.exists(self.STORAGE):
+            return []
+        with open(self.STORAGE, "r") as storage:
+            return json.load(storage)
+        
+    def save_storage(self, data):
+        with open(self.STORAGE, "w") as storage:
+            json.dump(data, storage, indent=4)
+
+    def register_image(self, image: ImageModel):
+        storage = self.load_storage()
+        storage.append({
+            "id": image.id,
+            "name": image.name,
+            "path": image.path,
+            "path2": image.path2
+        })
+
+        self.save_storage(storage)
+
+    def remove_image(self, image):
+        storage = self.load_storage()
+        storage.remove(image)
+
+        self.save_storage(storage)
